6720519f33f984e731f69c0bdd11e6fe083da31f
Ru-T <rwajnberg@gmail.com>
2015-10-01 14:42:14 -0400

    Create migration for steps table.

--- app/controllers/weights_controller.rb
+++ app/controllers/weights_controller.rb
@@ -29,10 +29,8 @@ class WeightsController < ApplicationController
     respond_to do |format|
       if @weight.save
         format.html { redirect_to @weight, notice: 'Weight was successfully created.' }
-        format.json { render :show, status: :created, location: @weight }
       else
         format.html { render :new }
-        format.json { render json: @weight.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -43,10 +41,8 @@ class WeightsController < ApplicationController
     respond_to do |format|
       if @weight.update(weight_params)
         format.html { redirect_to @weight, notice: 'Weight was successfully updated.' }
-        format.json { render :show, status: :ok, location: @weight }
       else
         format.html { render :edit }
-        format.json { render json: @weight.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -57,7 +53,6 @@ class WeightsController < ApplicationController
     @weight.destroy
     respond_to do |format|
       format.html { redirect_to weights_url, notice: 'Weight was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  get 'steps/view'
-
-  get 'steps/show'
-
-  get 'steps/index'
 
   resources :weights
+  resources :steps
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
