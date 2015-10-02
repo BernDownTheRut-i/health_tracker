afc00b7da8f935dd7a77b652358e6f798875ddc5
Ru-T <rwajnberg@gmail.com>
2015-10-02 12:19:43 -0400

    Basic methods on Steps model.

--- app/models/step.rb
+++ app/models/step.rb
@@ -1,7 +1,11 @@
 class Step < ActiveRecord::Base
 
-  # def self.sum_steps
-  #   self.all.reduce(0) {|sum, step| sum + step.amount}
-  # end
-  #
+  def self.sum_steps
+    self.all.reduce(0) {|sum, step| sum + step.number}
+  end
+
+  def self.step_count
+    self.count
+  end
+
 end
