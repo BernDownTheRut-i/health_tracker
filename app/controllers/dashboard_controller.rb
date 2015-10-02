a610ac4ad636205ae33fc476975bba4fa2dfdea7
David Bernheisel <david@bernheisel.com>
2015-10-02 15:30:04 -0400

    Add dashboard method for net calories

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,10 @@
 class DashboardController < ApplicationController
   def index
+    @calorics = Caloric.all
+    @exercises = Exercise.all
+    @exercise_types = ExerciseType.all
+    @steps = Step.all
+    @weights = Weight.all
   end
+
 end
--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -1,2 +1,7 @@
 class Dashboard < ActiveRecord::Base
+
+  def net_calories_today
+    @calories.consumed_today - @steps.steps_calories_today - @exercises.calories_burned_today
+  end
+
 end
