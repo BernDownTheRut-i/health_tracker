0398aa0906f5930d4b49734b92b6fb7983c6f1f5
David Bernheisel <david@bernheisel.com>
2015-10-02 16:45:08 -0400

    Change

--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -1,7 +1,7 @@
 class Dashboard < ActiveRecord::Base
 
-  def net_calories_today
-    @calories.consumed_today - @steps.steps_calories_today - @exercises.calories_burned_today
+  def net_calories_today(calorics, steps, exercises)
+    calorics.onsumed_today - steps.steps_calories_today - exercises.calories_burned_today
   end
 
 end
