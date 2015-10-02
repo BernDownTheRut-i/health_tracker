13d58e4112664059e77b1c1a5f3af51942a9b4cc
David Bernheisel <david@bernheisel.com>
2015-10-02 15:13:50 -0400

    Add consumed_today method to Caloric

--- app/models/caloric.rb
+++ app/models/caloric.rb
@@ -1,2 +1,10 @@
 class Caloric < ActiveRecord::Base
+
+  def self.consumed_today
+    cc = Caloric.where(date: Date.today).reduce(0) do |sum, c|
+      sum += c.number
+    end
+    cc.abs
+  end
+
 end
