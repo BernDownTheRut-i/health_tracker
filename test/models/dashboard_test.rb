fb9a4af2f543c1c59cbabd6c91a893fed84a47ed
Ru-T <rwajnberg@gmail.com>
2015-10-02 15:31:18 -0400

    Write test for net calories.

--- test/models/dashboard_test.rb
+++ test/models/dashboard_test.rb
@@ -3,8 +3,7 @@ require 'test_helper'
 class DashboardTest < ActiveSupport::TestCase
 
   test "test total calories consumed or burned today" do
-    
-
+    assert_equal (250 - 75 - 900), Dashboard.net_calories_today
   end
 
 end
