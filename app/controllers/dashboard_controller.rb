73e0962f2d31cbaa0dcc3c083c79569da91c89c7
Ru-T <rwajnberg@gmail.com>
2015-10-03 18:59:19 -0400

    Add metrics to dashboard.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,9 +2,11 @@ class DashboardController < ApplicationController
 
   def index
     @today = Dashboard.new(Date.today)
-
     @yesterday = Dashboard.new(Date.today - 1.day)
-
+    @steps_today = Step.steps_on_day(Date.today)
+    @calories_today = Caloric.consumed(Date.today)
+    @burned_today = Exercise.calories_burned(Date.today)
+    @steps_burned = Step.calories_burned(Date.today)
   end
 
 end
