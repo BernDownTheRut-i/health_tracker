a45ce991445782654659ec44b31d0ff2615addd0
Ru-T <rwajnberg@gmail.com>
2015-10-04 14:46:36 -0400

    Add forms onto dashboard view.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,6 +5,11 @@ class DashboardController < ApplicationController
 
     @yesterday = Dashboard.new(Date.today - 1.day)
 
+    @steps_today = Step.steps_on_day(Date.today)
+    @calories_today = Caloric.consumed(Date.today)
+    @burned_today = Exercise.calories_burned(Date.today)
+    @steps_burned = Step.calories_burned(Date.today)
+
     @caloric = Caloric.new
     @exercise = Exercise.new
     @step = Step.new
