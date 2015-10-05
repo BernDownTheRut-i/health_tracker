48209abb694055008aefc8b442582438a3b4c10b
Ru-T <rwajnberg@gmail.com>
2015-10-04 21:27:44 -0400

    Test coverage for today's metrics on dashboard.

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -7,4 +7,12 @@ class DashboardControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "show todays metrics" do
+    get :index
+    assert_response :success
+
+    assert response.body.include?("jogging")
+    assert response.body.include?("1500")
+  end
+
 end
